44986c0271bc827e7d17e221ef856e9864e688aa
Asxchksel <axel.mannheimer@elev.ga.ntig.se>
2023-10-16 09:51:58 +0200

    Students from specific town array list update

--- src/City.java
+++ src/City.java
@@ -1,10 +1,10 @@
-
 import java.util.ArrayList;
 
 public class City {
     private String country;
     private String cityName;
-    private ArrayList<Student> studentsFromTown;
+    private ArrayList<Student> studentsFromTown = new ArrayList<Student>();
+
 
     public City(String cityName, String country){
         this.cityName = cityName;
@@ -12,11 +12,18 @@ public class City {
     }
 
     public ArrayList<Student> getStudentsFromTown() {
+        Student student;
+        for (int i = 0; i < studentsFromTown.size(); i++) {
+            student = studentsFromTown.get(i);
+            System.out.println(student.getName() + " is from " + this.cityName);
+        }
         return studentsFromTown;
     }
 
     public void addStudentToTown(Student student){
+        this.studentsFromTown = studentsFromTown;
         studentsFromTown.add(student);
+
     }
 
     public void setStudentsFromTown(ArrayList<Student> studentsFromTown) {
--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner keyb = new Scanner(System.in);
         Student student1 = new Student("Axel", 42069);
         Student student2 = new Student("Sebastian", 14000);
         Student student3 = new Student("Loke", 21100);
         Student student4 = new Student("Jennifer", 15000);
+        System.out.println("What is the students name and id?: ");
+
 
         City city1 = new City("Stockholm", "Sweden");
         City city2 = new City("Helsinki", "Finlandia");
@@ -21,21 +24,20 @@ public class Main {
         city1.addStudentToTown(student1);
 
         student2.setHometown(city1);
-        //city1.addStudentToTown(student2);
+        city1.addStudentToTown(student2);
 
         student3.setHometown(city2);
-        //city2.addStudentToTown(student3);
+        city2.addStudentToTown(student3);
 
         student4.setHometown(city2);
-        //city2.addStudentToTown(student4);
+        city2.addStudentToTown(student4);
 
         student1.setBuddy(student2);
 
         city1.getStudentsFromTown();
-        System.out.println(city1.getStudentsFromTown());
+        city2.getStudentsFromTown();
 
-        student1.reportBuddyName();
-        System.out.println(student1.reportBuddyName());
+        System.out.println(student1.getName() + "'s buddy is " + student1.reportBuddyName());
 
     }
 
